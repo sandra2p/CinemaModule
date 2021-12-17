5227dbebe0f1b1642c943365664829e9618390dd
sandra2p <71703693+sandra2p@users.noreply.github.com>
2021-12-17 12:26:27 +0100

    Update actor.py

--- Cinema/models/actor.py
+++ Cinema/models/actor.py
@@ -5,5 +5,13 @@ class CinemaActor(models.Model):
     #name= fields.Char(string="Name")
     #age = fields.Float('Age', digits=(12, 1))
     movie_id= fields.Many2one('cinema.movie', string="Pelicula")
+    ganancias_peliculas=fields.Integer(string="$/movies")
+    ganancias_publi=fields.Integer(string="$/publi")
+    ganancias_interviews=fields.Integer(string="$/interviews") 
+    media_ganancias=fields.Float(string="ganacias medias por año", compute='_get_average') 
 
+    @api.depends('ganancias_peliculas', 'ganancias_publi', 'ganancias_interviews')
+    def _get_average(self):
+        for record in self:
+            record.media_ganancias = (record.ganancias_interviews + record.ganancias_publi + record.ganancias_peliculas ) / 3   
     
